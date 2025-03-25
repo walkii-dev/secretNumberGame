bd1afe2a9df38b3990bbe1d678694fde9de971ef
Lucas Oliveira <112091280+walkii-dev@users.noreply.github.com>
2025-03-25 19:41:52 -0300

    inserindo arquivos iniciais

---
+++ script.js
@@ -0,0 +1,28 @@
+let secretNumber = Math.floor(Math.random() * 10);
+console.log (secretNumber);
+
+function startGame() {
+    let guessArea = document.getElementById('guess');
+    guessArea.disabled = false; 
+
+    let start = document.querySelector('p');
+    start.innerHTML = 'Insira um número de 1 a 10 para começar.';
+
+    let buttonGuess = document.getElementById('botao');
+    buttonGuess.innerHTML = 'Chute';
+}
+function checkGuess(){
+    let guess = document.getElementById('guess').value;
+    console.log(guess);
+
+    if (guess == secretNumber){
+        alert('acertou carai');
+    } else{
+        alert('errou carai');
+    }
+    
+}
+
+// if text = disabled hover disabled too
+
+//mantain hover if textbox is clicked   
