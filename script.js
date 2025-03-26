847a0f2e0853255173c42e701eb378b2a410335a
Lucas Oliveira <lucasp.oliveira822@gmail.com>
2025-03-26 12:05:11 -0300

    correções feitas 260320251204

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-let secretNumber = Math.floor(Math.random() * 10);
-console.log (secretNumber);
+let secretNumber = Math.floor(Math.random() * 10); // define um número aleatório a ser criado 
+let firstExecution = true;
+
+
+function changeButton(){ //função que muda a funcionabilidade do botão, só que tem um bug aqui...
+document.getElementById('botao').addEventListener('click',function() {
+    if (firstExecution){
+        startGame();
+        firstExecution = false;
+    } else {
+        checkGuess();
+    }
+});
+}
 
 function startGame() {
-    let guessArea = document.getElementById('guess');
+    let guessArea = document.getElementById('guess'); 
     guessArea.disabled = false; 
 
-    let start = document.querySelector('p');
-    start.innerHTML = 'Insira um número de 1 a 10 para começar.';
+    useSubtitle('Insira um número de 1 a 10 para começar.');
 
     let buttonGuess = document.getElementById('botao');
     buttonGuess.innerHTML = 'Chute';
 }
+
 function checkGuess(){
+
     let guess = document.getElementById('guess').value;
-    console.log(guess);
+
+    document.getElementById('guess').value = 0; //isso aqui está errado, vou corrigir, deveria só fazer o valor sumir kk
 
     if (guess == secretNumber){
-        alert('acertou carai');
+        useSubtitle("Parabéns, você acertou!");
+
     } else{
-        alert('errou carai');
-    }
-    
+        useSubtitle("Eita, você errou!");
+    }  
 }
 
+function useSubtitle(text){ //função que faz a alteração de texto, futuramente devo aplicar um strong e uma estilização...
+    let tag = document.querySelector('p');
+    tag.innerHTML = text;
+}
+
+
+
+
 // if text = disabled hover disabled too
-//mais um comentário teste pra ver se o commit sobe
 //mantain hover if textbox is clicked   
