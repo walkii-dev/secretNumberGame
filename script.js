ed8276e45234d5c93b62e04c580c142690175e70
carlos caua <fostercarlos371@gmail.com>
2025-03-26 16:33:13 -0300

    problema do botão resolucionado, foi modificado a função de adicionar texto, adicionei um parâmetro tag, adicionei uma mensagem de saída caso o ususario acerte de primeira

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
-let secretNumber = Math.floor(Math.random() * 10); // define um número aleatório a ser criado 
+let secretNumber = geratorNumberSecret() // define um número aleatório a ser criado 
 let firstExecution = true;
+console.log(secretNumber)
+let attempts = 0
 
 
-function changeButton(){ //função que muda a funcionabilidade do botão, só que tem um bug aqui...
-document.getElementById('botao').addEventListener('click',function() {
-    if (firstExecution){
-        startGame();
-        firstExecution = false;
-    } else {
-        checkGuess();
-    }
-});
+function changeButton(){ //função que muda a funcionabilidade do botão, só que tem um bug aqui..., nao tem mais
+     firstExecution = false
+    useSubtitle("p",'Insira um número de 1 a 10 para começar.');
+     
+    let campo =document.getElementById("botao")
+    campo.innerText = "chute"    
+    
+    startGame()
+       
 }
 
-function startGame() {
-    let guessArea = document.getElementById('guess'); 
-    guessArea.disabled = false; 
-
-    useSubtitle('Insira um número de 1 a 10 para começar.');
-
-    let buttonGuess = document.getElementById('botao');
-    buttonGuess.innerHTML = 'Chute';
+function startGame  (){
+ let AreaInput = document.getElementById ('guess')
+ AreaInput.disabled = false
+ 
+if (AreaInput.value == 0){
+return;
+}else {
+    checkGuess()
 }
+}
+
 
 function checkGuess(){
 
     let guess = document.getElementById('guess').value;
+    if (attempts == 0 && guess == secretNumber){
+        useSubtitle("h1","Parabens, acertou de primeira")
+   
+    }else if (guess == secretNumber){
+        useSubtitle("h1","Parabéns, você acertou!");
 
-    document.getElementById('guess').value = 0; //isso aqui está errado, vou corrigir, deveria só fazer o valor sumir kk
+    }else if (attempts ==3){
+        useSubtitle("h1","Jogo do Número Secreto")
+        useSubtitle("p","o numero de tentativas foram atingidas")// 3 tentativas
+       guess.disabled = true
+        return guess.value = ""
 
-    if (guess == secretNumber){
-        useSubtitle("Parabéns, você acertou!");
-
-    } else{
-        useSubtitle("Eita, você errou!");
-    }  
+    }else {
+        useSubtitle("h1","Eita, você errou!,tente novamente");//alterei o texto adicionando o tente novamente
+        document.getElementById('guess').value = ""// com isso toda vez que errar, o numero vai sair.
+        attempts++// toda vez que errar uma tentativa é adicionada, assim quando chegar a 3, para o jogo, que é oque ocorre no else if acima.
+        console.log("o numero de tentativas é=",attempts)
+    }
 }
 
-function useSubtitle(text){ //função que faz a alteração de texto, futuramente devo aplicar um strong e uma estilização...
-    let tag = document.querySelector('p');
-    tag.innerHTML = text;
-}
 
+
+function useSubtitle(tag,text){// para fazer mudanças nos textos basta por a tag e o texto ex: useSubtitle("h1","parabens voce acertou o numero")
+    let campo = document.querySelector(tag)
+    campo.innerHTML = text
+   
+}
+function geratorNumberSecret(){
+
+    let campo = Math.floor(Math.random() * 10) + 1
+    return campo;
+}
 
 
 
